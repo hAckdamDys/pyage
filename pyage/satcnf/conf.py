c9e01b0880645f597f6a6873e38390d75742fe27
hAckdamDys <electro.ubro@gmail.com>
2018-03-16 08:36:28 +0100

    fix conf

--- pyage/satcnf/conf.py
+++ pyage/satcnf/conf.py
@@ -21,28 +21,32 @@ from pyage.satcnf.eval import SATEvaluation
 from pyage.satcnf.init import EmasInitializer, root_agents_factory, SATGenotypeInitializer, SATGenotypeInitializer2
 from pyage.satcnf.mutation import Mutation
 from pyage.satcnf.naming_service import NamingService
+from pyage.satcnf.load_values import load_values
 from pyage.satcnf.selection import TournamentSelectionV2
-from pyage.solutions.evolution.initializer import FloatInitializer
 
 from pyage.solutions.evolution.selection import TournamentSelection
+
+
 
 logger = logging.getLogger(__name__)
 
 
 
-cnf_data=pd.read_csv("50var-80claus-satisable.cnf",skiprows=11,header=None,sep=" ",usecols=[0,1,2]).values
+# cnf_data=pd.read_csv("50var-80claus-satisable.cnf",skiprows=11,header=None,sep=" ",usecols=[0,1,2]).values
 
-number_of_booleans=cnf_data.max() # 50 since clauses have values from 1 to 50, for each variable
-number_of_clauses=len(cnf_data) # 80
+cnf_data,number_of_booleans,number_of_clauses=load_values("zebra_v155_c1135.cnf")
+
+# number_of_booleans=cnf_data.max() # 50 since clauses have values from 1 to 50, for each variable
+# number_of_clauses=len(cnf_data) # 80
 
 agents_count = 2
 logger.debug("EMAS, %s agents", agents_count)
 
 agents = generate_agents("agent", agents_count, Agent)
 
-stop_condition = lambda: StepLimitStopCondition(1600)
+stop_condition = lambda: StepLimitStopCondition(100)
 
-size = 500
+size = 250
 seed = 0
 operators = lambda: [SATEvaluation(cnf_data), TournamentSelectionV2(size=10, tournament_size=10),
                      SATCrossover(size=100, max_fitness=number_of_clauses,worse_factor=0.1), Mutation(probability=0.1, evol_probability=0.1)]
